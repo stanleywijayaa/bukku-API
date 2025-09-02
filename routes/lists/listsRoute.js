6397e7c4b2914c38534a4e88dc41828191d0b575
Realxeng <realxengpandelaki@gmail.com>
2025-09-02 11:46:43 +0800

    created sales route, added listroute to export

--- routes/lists/listsRoute.js
+++ routes/lists/listsRoute.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const listsController = require('../../logics/lists/listsLogic');
+const { route } = require("../sales/salesRoute");
 
 router.route('/')
     .post(listsController.getLists)
+
+module.exports = router
---
+++ routes/sales/salesRoute.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+
+router.route('/quotation')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.route('/salesorder')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.route('/deliveryorder')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.route('/invoice')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.route('/creditnote')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.route('/payment')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.route('/refund')
+    .post()
+    .get()
+    .put()
+    .patch()
+    .delete()
+
+router.use('/all', (() => {
+    const allRoute = express.Router()
+    allRoute.get('/quotation')
+    allRoute.get('/salesorder')
+    allRoute.get('/deliveryorder')
+    allRoute.get('/invoice')
+    allRoute.get('/creditnote')
+    allRoute.get('/payment')
+    allRoute.get('/refund')
+    return allRoute
+}))
+
+module.exports = router
