20b12bc9c74cd699a9d419cde83bfbf249f52e61
Realxeng <realxengpandelaki@gmail.com>
2025-09-03 16:13:21 +0800

    validate get sales request, added function to only fetch the specified page in get all sales

--- logics/sales/salesLogic.js
+++ logics/sales/salesLogic.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const dotenv = require('dotenv');
 dotenv.config();
 
+//API call template
 const api = axios.create({
     baseURL: `${process.env.BUKKU_API_URL}sales/`,
     headers: {
@@ -40,40 +41,42 @@ const createSales = async (req,res) => {
 const getSalesList = async (req,res) => {
     //Determine the sales type
     const { type } = req.params
-    //Allowed parameters
-    const allowedParams = [
-        "search", "custom_search", "contact_id",
-        "date_from", "date_to", "status",
-        "email_status", "transfer_status",
-        "page", "page_size", "sort_by", "sort_dir"
-    ];
+    //Get the raw params
+    const rawParams = req.query
+    //Request validation
+    const {valid, params} = verifyGetRequest(rawParams, type)
+    if (!valid.bool){
+        return res.status(valid.status).json({message: valid.message})
+    }
 
-    //Filter invalid parameters
-    let params = Object.keys(req.query)
-    .filter(key => allowedParams.includes(key))
-    .reduce((obj, key) => {
-        obj[key] = req.query[key];
-        return obj;
-    }, {});
-
+    //Get the data
     try{
-        let page = 1
-        let totalPage = 1
         let data = []
-        do {
-            //Update page
-            params.page = page
+        //Get only the page requested
+        if (params.page) {
             //Get the data
             const response = await api.get(type, { params })
             const result = response.data
+            //Append the data to data object
             data.push(...result.transactions)
-            //Get the total number of page
-            totalPage = Math.ceil(result.paging.total / result.paging.per_page)
-            //Iterate page
-            page++
         }
-        //Get all the data
-        while(page <= totalPage)
+        //Get all pages if page is not specified
+        else {
+            let page = 1
+            let totalPage = 1
+            do {
+                //Get the data
+                const response = await api.get(type, { ...params, page })
+                const result = response.data
+                data.push(...result.transactions)
+                //Get the total number of page
+                totalPage = Math.ceil(result.paging.total / result.paging.per_page)
+                //Iterate page
+                page++
+            }
+            //Get all the data
+            while(page <= totalPage)
+        }
         //Return the data
         res.status(200).json({ data })
     }
@@ -245,6 +248,7 @@ const deleteSales = async (req,res) => {
     }
 }
 
+//Verify request for creating sales entry
 function verifyCreateRequest(data, type){
     //Check for required general parameters
     if(!data.contact_id ||
@@ -260,9 +264,9 @@ function verifyCreateRequest(data, type){
         if(data.tag_ids && data.tag_ids.length > 4) return {bool: false, status: 400, message: "Invalid tag"}
         if(!(/^\d{4}-\d{2}-\d{2}$/.test(data.date))) return {bool: false, status: 400, message: "Invalid date"}
         if(data.description && data.description.length > 255) return {bool: false, status: 400, message: "Invalid description"}
-        if(!(new Intl.NumberFormat("en", {style: 'currency', currency: data.currency_code}))) return {bool: false, status: 400, message: "Invalid currency code"}
+        try{new Intl.NumberFormat("en", {style: 'currency', currency: data.currency_code})} catch { return {bool: false, status: 400, message: "Invalid currency code"} }
         if(!(["draft", "pending_approval", "ready"].includes(data.status))) return {bool: false, status: 400, message: "Invalid status"}
-        if((data.number || data.number2) && (data.number.length > 50 || data.number2.length > 50)) return {bool: false, status: 400, message: "Invalid transaction or reference number"}
+        if((data.number && data.number.length > 50) || (data.number2 && data.number2.length > 50)) return {bool: false, status: 400, message: "Invalid transaction or reference number"}
     }
 
     //Check for type-specific parameters
@@ -279,33 +283,93 @@ function verifyCreateRequest(data, type){
             return {bool: true}
         }
     }
+    //Check required parameter for payments
     else if (type === 'payments'){
-        //Check required parameter for payments
         if(!data.amount || !data.deposit_items) return {bool: false, status: 400, message: "Missing required parameter(s)"}
         return {bool: true}
     }
+    //Check required parameter for refunds
     else if (type === 'refunds'){
-        //Check required parameter for refunds
         if(!data?.deposit_items) return {bool: false, status: 400, message: "Missing required parameter(s)"}
         return {bool: true}
     }
+    //Handle invalid route
     else {
         return {bool: false, status: 404, message: "Invalid request"}
     }
 }
 
-function verifyGetRequest(body,type){
-    if (type === 'quotes' || type === 'orders' || type === 'delivery_orders' || type === 'invoice' || type === 'credit_notes'){
-        
-    }
-    else if (type === 'payments'){
+//Verify request for searching sales entry
+function verifyGetRequest(rawParams, type){
+    //Set the allowed filter parameters
+    const allowedParams = [
+        "search", "custom_search", "contact_id",
+        "date_from", "date_to", "status", "payment_status",
+        "email_status", "transfer_status", "payment_mode",
+        "page", "page_size", "sort_by", "sort_dir", "account_id"
+    ];
+    //Get all valid query parameters
+    let params = Object.keys(rawParams)
+    .filter(key => allowedParams.includes(key))
+    .reduce((obj, key) => {
+        obj[key] = rawParams[key];
+        return obj;
+    }, {});
+    
+    //Validate general parameters
+    if(params.search && params.search.length > 100) return {bool: false, status: 400, message: "Invalid search length"}
+    if(params.custom_search && params.custom_search.length > 100) return {bool: false, status: 400, message: "Invalid custom search length"}
+    if(!(/^\d{4}-\d{2}-\d{2}$/.test(params.date_from))) return {bool: false, status: 400, message: "Invalid from date"}
+    if(!(/^\d{4}-\d{2}-\d{2}$/.test(params.date_to))) return {bool: false, status: 400, message: "Invalid end date"}
+    if(params.status && !(["draft", "pending_approval", "ready", "void"].includes(params.status))) return {bool: false, status: 400, message: "Invalid status"}
+    if(params.payment_status && !(['paid', 'outstanding'].includes(params.payment_status))) return {bool: false, status: 400, message: "Invalid payment status"}
+    if(params.email_status && !(["UNSENT", "PENDING", "SENT", "BOUNCED", "OPENED", "VIEWED"].includes(params.email_status))) return {bool: false, status: 400, message: "Invalid email status"}
+    if(page && page < 1) return {bool: false, status: 400, message: "Invalid page"}
+    if(params.sort_dir && !(['asc', 'desc'].includes(params.sort_dir))) return {bool: false, status: 400, message: "Invalid payment status"}
+    if(params.sort_by && !(["number", "date", "contact_name", "number2", "title", "description", "amount", "balance", "created_at"].includes(params.sort_by))) return {bool: false, status: 400, message: "Invalid sort type"}
 
+    //##Validate type-specific parameters##
+    //Validate quotes, orders and delivery orders request
+    if (type === 'quotes' || type === 'orders' || type === 'delivery_orders'){
+        //Validate parameters
+        if(params.transfer_status && !(["ALL", "NOT_TRANSFERRED", "PARTIAL_TRANSFERRED", "TRANSFERRED"].includes(params.transfer_status))) return {bool: false, status: 400, message: "Invalid transfer status"}
+        if(params.sort_by && params.sort_by === 'balance') return {bool: false, status: 400, message: "Invalid sort type"}
+        //Filter invalid parameters
+        const filteredParams = params.filter(param => !['payment_status', 'payment_mode', 'account_id'].includes(param))
+        return {bool: true, params: filteredParams}
+    }
+    //Validate invoice request
+    else if (type === 'invoice'){
+        //Validate parameters
+        if(params.payment_mode && !(["credit", "cash"].includes(params.payment_mode))) return {bool: false, status: 400, message: "Invalid payment mode"}
+        //Filter invalid parameters
+        const filteredParams = params.filter(param => !['transfer_status', 'account_id'].includes(param))
+        return {bool: true, params: filteredParams}
+    }
+    //Validate credit notes request
+    else if (type === 'credit_notes'){
+        //Filter invalid parameters
+        const filteredParams = params.filter(param => !['transfer_status', 'payment_mode', 'account_id'].includes(param))
+        return {bool: true, params: filteredParams}
+    }
+    //Validate payments requests
+    else if (type === 'payments'){
+        //Validate parameters
+        if(params.sort_by && params.sort_by === 'title') return {bool: false, status: 400, message: "Invalid sort type"}
+        //Filter invalid parameters
+        const filteredParams = params.filter(param => !['transfer_status', 'payment_mode'].includes(param))
+        return {bool: true, params: filteredParams}
     }
     else if (type === 'refunds'){
-
+        //Validate parameters
+        if(params.sort_by && !(["amount", "balance"].includes(params.sort_by))) return {bool: false, status: 400, message: "Invalid sort type"}
+        //Filter invalid parameters
+        const filteredParams = params.filter(param => !['transfer_status', 'payment_mode'].includes(param))
+        return {bool: true, params: filteredParams}
     }
+    //Handle invalid requests
     else {
-        return null
+        return {bool: false, status: 404, message: "Invalid request"}
     }
 }
 
