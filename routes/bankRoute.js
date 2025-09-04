9687277839f284fff4c15cd9f63b9b1ae3aae99c
stanleywijayaa <stanleytan2018@gmail.com>
2025-09-04 12:19:41 +0800

    created routing for transfer

--- routes/bankRoute.js
+++ routes/bankRoute.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const moneyInController = require('../logics/bank/moneyInLogic')
+const moneyOutController = require('../logics/bank/moneyOutLogic')
+const transfersController = require('../logics/bank/transfersLogic')
 
 router.route('/incomes')
     .post(moneyInController.createMoneyIn)
@@ -9,5 +11,25 @@ router.route('/incomes')
     .patch(moneyInController.updateMoneyInStatus)
     .delete(moneyInController.deleteMoneyIn)
 
+router.route('/expenses')
+    .post(moneyOutController.createMoneyOut)
+    .get(moneyOutController.getMoneyOutList)
+    .put(moneyOutController.updateMoneyOut)
+    .patch(moneyOutController.updateMoneyOutStatus)
+    .delete(moneyOutController.deleteMoneyOut)
+
+router.route('/transfers')
+    .post(transfersController.createTransaction)
+    .get(transfersController.getTransferList)
+    .put(transfersController.updateTransfer)
+    .patch(transfersController.updateTransferStatus)
+    .delete(transfersController.deleteTransfer)
+
 router.route('/incomes/:id')
     .get(moneyInController.getMoneyIn)
+
+router.route('/expenses/:id')
+    .get(moneyOutController.getMoneyOut)
+
+router.route('/transfers/:id')
+    .get(transfersController.getTransfer)
